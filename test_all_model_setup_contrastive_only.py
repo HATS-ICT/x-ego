6143caf84dc99bacbb4ac231bae682b02e887bad
Yunzhe Wang <wangyz1999@hotmail.com>
2026-04-25 21:39:46 -0700

    chore: fix contrastive virtual batch settings

--- test_all_model_setup_contrastive_only.py
+++ test_all_model_setup_contrastive_only.py
@@ -35,10 +35,10 @@ CHECK_TORCH_COMPILE = sys.platform.startswith("linux")
 
 TRAINING_SETTINGS = {
     "clip": (128, 8),
-    "siglip2": (64, 32),
-    "dinov3": (32, 64),
-    "vjepa2": (32, 64),
-    "resnet50": (32, 64),
+    "siglip2": (64, 16),
+    "dinov3": (32, 32),
+    "vjepa2": (32, 32),
+    "resnet50": (32, 32),
 }
 
 
